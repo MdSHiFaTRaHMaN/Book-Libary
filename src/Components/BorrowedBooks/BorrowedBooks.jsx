fac54ac6df4785907e4a452a65ab059aa1421176
ShifatRahman420 <copyremc4@gmail.com>
2023-12-19 00:19:23 +0600

    Navber Style

--- src/Components/BorrowedBooks/BorrowedBooks.jsx
+++ src/Components/BorrowedBooks/BorrowedBooks.jsx
@@ -4,17 +4,17 @@ import BorrowerData from "./BorrowerData";
 import Swal from "sweetalert2";
 
 const BorrowedBooks = () => {
-    
-    const {user} = useContext(AuthContext);
+
+    const { user } = useContext(AuthContext);
     const [borrowing, setBorrowing] = useState([]);
     const url = `https://book-library-server-two.vercel.app/borrowings?email=${user.email}`;
-    useEffect( () =>{
-        fetch(url, {credentials: 'include'})
-        .then(res => res.json())
-        .then(data => setBorrowing(data))
-    },[url])
-    
-    const handleDelete = id =>{
+    useEffect(() => {
+        fetch(url, { credentials: 'include' })
+            .then(res => res.json())
+            .then(data => setBorrowing(data))
+    }, [url])
+
+    const handleDelete = id => {
         const proceed = Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
@@ -23,89 +23,89 @@ const BorrowedBooks = () => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             console.log(result)
             Swal.fire({
                 title: "Deleted!",
                 text: "Your file has been deleted.",
                 icon: "success"
-              });
-          });
-          if(proceed){
-            fetch(`https://book-library-server-two.vercel.app/borrowings/${id}`,{
+            });
+        });
+        if (proceed) {
+            fetch(`https://book-library-server-two.vercel.app/borrowings/${id}`, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if(data.deletedCount > 0){
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Your file has been deleted.",
-                        icon: "success"
-                      });
-                      const remaining = borrowing.filter(borrow => borrow._id !== id);
-                      setBorrowing(remaining);
-                }
-            })
-          }
-          
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                        const remaining = borrowing.filter(borrow => borrow._id !== id);
+                        setBorrowing(remaining);
+                    }
+                })
+        }
+
     }
     return (
         <div>
             <h2 className=" text-center text-blue-700 text-3xl ">Borrow Book: {borrowing.length}</h2>
             <section className="container px-4 mx-auto min-h-screen" >
-    
-    <div className="flex flex-col mt-6">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <thead className="bg-gray-50 dark:bg-gray-800">
-                            <tr>
-                                <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    <div className="flex items-center gap-x-3">
-                                        <span>Image</span>
-                                    </div>
-                                </th>
 
-                                
+                <div className="flex flex-col mt-6">
+                    <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                    <thead className="bg-gray-50 dark:bg-gray-800">
+                                        <tr>
+                                            <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                <div className="flex items-center gap-x-3">
+                                                    <span>Image</span>
+                                                </div>
+                                            </th>
 
-                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    <button className="flex items-center gap-x-2">
-                                        <span>Borrowed Date</span>
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="w-4 h-4">
-                                            <path  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                        </svg>
-                                    </button>
-                                </th>
 
-                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Return Date</th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                <button className="flex items-center gap-x-2">
+                                                    <span>Borrowed Date</span>
 
-                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Emails</th>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4">
+                                                        <path d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                                                    </svg>
+                                                </button>
+                                            </th>
 
-                                <th scope="col" className="relative py-3.5 px-4">
-                                    <span className="sr-only">Edit</span>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                           {
-                            borrowing.map(borrowData => <BorrowerData 
-                                key={borrowData._id} 
-                                borrowData={borrowData}
-                                handleDelete={handleDelete}
-                                ></BorrowerData>)
-                           }
-                        </tbody>
-                    </table>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Return Date</th>
+
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Emails</th>
+
+                                            <th scope="col" className="relative py-3.5 px-4">
+                                                <span className="sr-only">Edit</span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                                        {
+                                            borrowing.map(borrowData => <BorrowerData
+                                                key={borrowData._id}
+                                                borrowData={borrowData}
+                                                handleDelete={handleDelete}
+                                            ></BorrowerData>)
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
-</section>
+            </section>
         </div>
     );
 };
--- src/LawOutPage/Navber/Navber.jsx
+++ src/LawOutPage/Navber/Navber.jsx
@@ -48,12 +48,30 @@ const Navber = () => {
                 <div className="navbar-end">
                     {
                         user ?
-                            <div className='flex items-center'>
-                                <div className='text-center hidden lg:block'>
-                                    <h4>{user?.displayName}</h4>
-                                    <p>{user?.email}</p>
+                            <div className="dropdown dropdown-bottom dropdown-end">
+                                <div className="avatar m-1" tabIndex={0}>
+                                    <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <img src='https://i.ibb.co/C9GVgbx/Screenshot-2023-11-26-112111.png' />
+                                    </div>
                                 </div>
-                                <button onClick={handleSignOut} className="btn text-white font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 ml-1">Sign Out</button>
+                                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                    <a className="flex items-center pt-2 text-sm text-gray-600 transition-colors duration-300 transform hover:bg-gray-10">
+                                        <img className="flex-shrink-0 object-cover rounded-full w-9 h-9" src="https://i.ibb.co/C9GVgbx/Screenshot-2023-11-26-112111.png" alt="jane avatar" />
+                                        <div className="mx-1">
+                                            <h1 className="text-sm font-semibold text-gray-700">{user?.displayName}</h1>
+                                            <p className="text-sm text-gray-500">{user?.email}</p>
+                                        </div>
+                                        <div className="divider"></div>
+                                    </a>
+                                    <a onClick={handleSignOut} className="flex items-center p-3 text-sm  text-blue-900 capitalize transition-colors duration-300 transform hover:bg-gray-300 hover:rounded-xl">
+                                        <svg className="w-5 h-5 mx-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M19 21H10C8.89543 21 8 20.1046 8 19V15H10V19H19V5H10V9H8V5C8 3.89543 8.89543 3 10 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21ZM12 16V13H3V11H12V8L17 12L12 16Z" fill="currentColor"></path>
+                                        </svg>
+                                        <span className="mx-1 font-semibold">
+                                            Sign Out
+                                        </span>
+                                    </a>
+                                </ul>
                             </div>
                             :
                             <Link to="/login">
